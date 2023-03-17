35d33f5f8bc0f95f70268db9c2ca20fc116e3515
admin <admin@RAVITEJAAA>
2023-03-17 09:42:24 +0530

    Constractors

---
+++ src/com/vagdeviitstudents/ravi/Emplyee.java
@@ -0,0 +1,18 @@
+package com.vagdeviitstudents.ravi;
+
+class Emplyee 
+{
+   String empName;
+   long empSalary;
+Emplyee()
+{
+	System.out.println("Emplyee is Created");
+}
+Emplyee(String n,long s)
+{
+	this.empName=n;
+	this.empSalary=s;
+	System.out.println("Emplyee Name :"+n);
+	System.out.println("Emplyee Salary :"+s);
+}
+}
---
+++ src/com/vagdeviitstudents/ravi/Super.java
@@ -0,0 +1,13 @@
+package com.vagdeviitstudents.ravi;
+
+public class Super 
+  {
+    public static void main(String args[]) 
+		{
+			Emplyee R = new Emplyee();	
+			Emplyee Z = new Emplyee("Ravi",45000);
+		    
+		}
+	}
+
+
