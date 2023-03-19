3b9bddd8c39c5470b53eb305b498ed96275adf89
User <User@WINDOWS-VSJSTE8>
2023-03-19 10:30:11 +0530

    naveen changes

---
+++ src/com/vagdeviitstudents/naveen/ConstructorMainClass.java
@@ -0,0 +1,11 @@
+package com.vagdeviitstudents.naveen;
+
+public class ConstructorMainClass {
+	public static void main(String[] args) 
+	{
+		//Employer e = new Employer();// Object Creation. object is calling Default Constructor
+		Employer e = new Employer("Naveen",26);// Parameterized constructor
+		//ItEmployer e1 = new ItEmployer(); // Object Creation. object is calling parent calls
+	}
+
+}
