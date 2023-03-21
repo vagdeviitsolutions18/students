a7e33216f68178f6703ecb9418eeceebcf451e4b
User <User@WINDOWS-VSJSTE8>
2023-03-21 18:49:32 +0530

    series number concept

---
+++ src/com/vagdeviitstudents/naveen/Primenumber.java
@@ -0,0 +1,40 @@
+package com.vagdeviitstudents.naveen;
+
+//public class Evennumbers {
+//	int i = 1;
+//
+//	public static void main(String[] args) {
+//
+//		for (int i = 1; i <= 10; i++) {
+//			if (i % 2 == 0) {
+//				System.out.println(i + " is even");
+//			}
+//		}
+//
+//	}
+//}
+
+
+public class Primenumber  {
+	int i = 2;
+	public static void main(String[] args) {
+		
+		for (int i = 2; i <= 99; i++) {
+			boolean prime =true;
+            for (int j = 2; j < i; j++) {
+
+			if (i%2==0) {
+				boolean isprime =false;
+				break;
+				
+			}
+			}
+				
+			if(prime) {
+				System.out.println("is prime"+i);
+			}
+		}
+		}
+	}
+
+
