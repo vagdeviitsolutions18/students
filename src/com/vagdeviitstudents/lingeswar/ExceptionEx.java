adfd56210f957e68e783f7416b6a59c441bb5698
admin <admin@RAVITEJAAA>
2023-03-22 18:39:05 +0530

    ExceptionHandling Programs.lingeswar

---
+++ src/com/vagdeviitstudents/lingeswar/ExceptionEx.java
@@ -0,0 +1,25 @@
+package com.vagdeviitstudents.lingeswar;
+
+public class ExceptionEx {
+	public static void main(String args[])throws Exception
+	{
+
+		int a[] = new int[2];
+		try 
+		{
+			System.out.println("Access element three :" + a[3]);
+		} 
+		catch (ArrayIndexOutOfBoundsException e) 
+		{
+			System.out.println("Exception thrown :" + e);
+			throw new ArrayIndexOutOfBoundsException("Error");
+		} 
+		finally 
+		{
+			a[0] = 6;
+			System.out.println("First element value: " + a[0]);
+			System.out.println("The finally statement is executed");
+		}
+	}
+}
+
---
+++ src/com/vagdeviitstudents/lingeswar/javaExceptionHandling.java
@@ -0,0 +1,32 @@
+package com.vagdeviitstudents.lingeswar;
+
+public class javaExceptionHandling 
+{
+
+	public static void main(String args[])
+	{  
+		System.out.println("Program execution start");
+		int  fNumber=15;
+		int  sNumber=0;
+		int result=0;
+		try
+		{  
+
+			result=fNumber/sNumber; 
+		}
+
+		catch(ArithmeticException ae)
+		{
+			System.out.println(ae.toString());
+			throw ae;
+		}
+		finally
+		{
+			System.out.println("finally block");
+		}
+		System.out.println("Output is :"+result);
+		System.out.println("Program execution end");  
+	}  
+}  
+
+
