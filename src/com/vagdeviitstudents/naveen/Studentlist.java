0af1e0ea7c49ee4998dd26c2ff1c05bef11c4bdd
User <User@WINDOWS-VSJSTE8>
2023-03-25 14:46:22 +0530

    Arraylist

--- src/com/vagdeviitstudents/naveen/Studentlist.java
+++ src/com/vagdeviitstudents/naveen/Studentlist.java
@@ -14,31 +14,28 @@ public class Studentlist {
 
 	}
 
-	//to sting for above student list 
+	// to sting for above student list
 	public String toString() {
 		return "Studentlist [name=" + name + ", age=" + age + "]";
 	}
 
 	public static void main(String[] args) {
-		
-		Studentlist s1=new Studentlist("ravi",22);
-		Studentlist s2=new Studentlist("raju",22);
-		Studentlist s3=new Studentlist("naveen",29);
-		Studentlist s4=new Studentlist("anna",15);
-		
-	
-		
-	ArrayList<Studentlist> list = new ArrayList<Studentlist>();
-	list.add(s1);
-	list.add(s2);
-	list.add(s3);
-	list.add(s4);
-	
-	System.out.println(list);// print the above details 
-	for(Studentlist user :list) {
-		System.out.println(user); // basic define to print the details in order 
-	}
-	
 
-}
+		Studentlist s1 = new Studentlist("ravi", 22);
+		Studentlist s2 = new Studentlist("raju", 22);
+		Studentlist s3 = new Studentlist("naveen", 29);
+		Studentlist s4 = new Studentlist("anna", 15);
+
+		ArrayList<Studentlist> list = new ArrayList<Studentlist>();
+		list.add(s1);
+		list.add(s2);
+		list.add(s3);
+		list.add(s4);
+
+//	System.out.println(list);// print the above details 
+		for (Studentlist user : list) {
+			System.out.println(user); // basic define to print the details in order
+		}
+
+	}
 }
